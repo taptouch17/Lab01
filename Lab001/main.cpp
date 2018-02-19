eb98c7f3379bc4fde946b78ede9a99d4c65f009e
taptouch17 <31634464+taptouch17@users.noreply.github.com>
2018-02-18 20:25:52 -0400

    Initial Commit

---
+++ Lab001/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab001
+//
+//  Created by Jelani on 18/02/2018.
+//  Copyright © 2018 Tap. TOUCH. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
